aaea25753b2ca60811d7842137b19cba59aded96
DipShtick <vancouver_cliparf@simplelogin.com>
2023-07-06 12:27:24 +0100

    DataManager functions

--- Scripts/Editor/DataManager.cs
+++ Scripts/Editor/DataManager.cs
@@ -5,25 +5,76 @@ using System;
 public partial class DataManager : Node
 {
     const string PROPERTY_FILE = "Data/Properties.JSON";
-    static Dictionary<string, Dictionary<string, Array<string>>> OpenPropertyFile()
+    static Dictionary<string,Dictionary<string,Dictionary<string, Variant>>> OpenPropertyFile()
     {
         using var file = FileAccess.Open(PROPERTY_FILE, FileAccess.ModeFlags.Read);
 
-        Dictionary<string,Dictionary<string,Array<string>>> data = new Dictionary<string, Dictionary<string, Array<string>>>();
-        data = (Dictionary<string,Dictionary<string,Array<string>>>)Json.ParseString(file.GetAsText());
+        Dictionary<string,Dictionary<string,Dictionary<string, Variant>>> data = new Dictionary<string,Dictionary<string,Dictionary<string, Variant>>>();
+        data = (Dictionary<string,Dictionary<string,Dictionary<string, Variant>>>)Json.ParseString(file.GetAsText());
 
         return data;
     }
 
-    /*
     public static string GetPropertyName(string internalPropName)
     {
         var data = OpenPropertyFile();
+        string propertyName = null;
 
         if (data.ContainsKey(internalPropName))
-            return data[internalPropName][yes];
+            return propertyName = (string)data[internalPropName]["ID"]["Name"];
 
-        return null;
+        return propertyName;
     }
-    */
+
+    public static int GetPropertyPrice(string internalPropName)
+    {
+        var data = OpenPropertyFile();
+        int value = 0;
+
+        if (data.ContainsKey(internalPropName))
+        {
+            value = (int)data[internalPropName]["Costs"]["PurchasePrice"];
+        }
+
+        return value;
+    }
+
+    public static int GetPropertyBuildcost(string internalPropName)
+    {
+        var data = OpenPropertyFile();
+        int value = 0;
+
+        if (data.ContainsKey(internalPropName))
+        {
+            value = (int)data[internalPropName]["Costs"]["BuildCost"];
+        }
+
+        return value;
+    }
+
+    public static int GetPropertyMortgage(string internalPropName)
+    {
+        var data = OpenPropertyFile();
+        int value = 0;
+
+        if (data.ContainsKey(internalPropName))
+        {
+            value = (int)data[internalPropName]["Costs"]["Mortgage"];
+        }
+
+        return value;
+    }
+
+    public static int GetPropertyRent(string internalPropName)
+    {
+        var data = OpenPropertyFile();
+        int value = 0;
+
+        if (data.ContainsKey(internalPropName))
+        {
+            value = (int)data[internalPropName]["RentValues"]["Rent"];
+        }
+
+        return value;
+    }
 }
