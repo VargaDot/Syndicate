435ef560465bc117d737d9eb7fded8286a94a6fc
DipShtick <vancouver_cliparf@simplelogin.com>
2023-11-10 16:56:28 +0100

    Dice

--- src/ui/game/Dice.gd
+++ src/ui/game/Dice.gd
@@ -1,4 +1,15 @@
 extends Control
-
-func _onRequest():
-	pass # Replace with function body.
+ 
+func _onRequest(diceRoll):
+	if diceRoll > 12: printerr("Invalid dice roll outcome")
+	
+	var x = diceRoll - 2
+	if x == 0: pass
+	else: x / 2
+	
+	var result
+	if x % 1 != 0: result = x + 1
+	
+	show()
+	$Dice1.play("rolling")
+	$Dice2.play("rolling")
