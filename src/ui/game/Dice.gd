26ece96eda4722968f4d213746de00b248974d89
DipShtick <vancouver_cliparf@simplelogin.com>
2023-11-11 18:34:41 +0100

    Linked dice node with timer child

--- src/ui/game/Dice.gd
+++ src/ui/game/Dice.gd
@@ -4,12 +4,35 @@ func _onRequest(diceRoll):
 	if diceRoll > 12: printerr("Invalid dice roll outcome")
 	
 	var x = diceRoll - 2
-	if x == 0: pass
-	else: x / 2
+	x /= 2
 	
 	var result
 	if x % 1 != 0: result = x + 1
 	
+	_playAnimation()
+
+func _playAnimation():
+	_launchTimer()
+	
 	show()
+	
 	$Dice1.play("rolling")
 	$Dice2.play("rolling")
+
+func _launchTimer():
+	var t:float = randf_range(1.25,3.25)
+	$Timer.wait_time = t;
+	$Timer.start()
+
+var ranOnce:bool = false
+func _onTimeout():
+	if ranOnce == false:
+		var s:float = randf_range(0.25,0.75)
+		$Dice1.speed_scale = s
+		$Dice2.speed_scale = s
+		
+		_launchTimer()
+		ranOnce = true
+	else:
+		
+		ranOnce = false
