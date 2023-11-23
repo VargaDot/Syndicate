dc7363896b322e071616e6be9ba9339385cdd532
DipShtick <vancouver_cliparf@simplelogin.com>
2023-11-23 17:33:22 +0100

    Update MusicPlayer.cs
    delete the ready comment

--- src/bcg/MusicPlayer.cs
+++ src/bcg/MusicPlayer.cs
@@ -3,14 +3,13 @@ using Godot.Collections;
 
 public partial class MusicPlayer : AudioStreamPlayer
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() { MusicSelector(); }
 
 	byte previousSong = 0;
 	void MusicSelector()
 	{
 		byte x = 0;
-		
+
 		if (MusicList.Count <= 0) GD.PushWarning("No music exists");
 		while (x == previousSong) x = (byte)GD.RandRange(0, MusicList.Count - 1);
 
