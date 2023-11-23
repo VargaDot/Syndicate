8c0253a89571cf42c5936c873bd0bffba3a9e7cc
DipShtick <vancouver_cliparf@simplelogin.com>
2023-11-23 16:41:10 +0100

    removed static
    The issue with autoloaded scripts stemmed from this

--- Data/Khana.cs
+++ Data/Khana.cs
@@ -2,8 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 
-[GlobalClass]
-public partial class Khana : CSharpScript
+public partial class Khana : Node
 {
   struct Agent
   {
@@ -35,56 +34,56 @@ public partial class Khana : CSharpScript
     }
   }
   
-  private static readonly List<Agent> Daftar = new();
-  public static void AddAgent(byte ID, ushort startingCash, string username)
+  private readonly List<Agent> Daftar = new();
+  public void AddAgent(byte ID, ushort startingCash, string username)
   {
     Daftar.Add(new Agent(ID, startingCash, username));
   }
 
-  public static void RemoveAgent(byte AgentID) 
+  public void RemoveAgent(byte AgentID) 
   {
     Agent agentToRemove = Daftar.Find(agent => agent.ID == AgentID);
     Daftar.Remove(agentToRemove);
   }
 
-  public static void ConductTransaction(byte AgentID, short amount)
+  public void ConductTransaction(byte AgentID, short amount)
   {
     Agent selectedAgent = Daftar.Find(agent => agent.ID == AgentID);
     selectedAgent.Cash += amount;
   }
 
-  public static void ExchangeLand(byte BuyerID, byte SellerID, byte PropertyID)
+  public void ExchangeLand(byte BuyerID, byte SellerID, byte PropertyID)
   {
     RemoveProperty(SellerID, PropertyID);
     AddProperty(BuyerID, PropertyID);
   }
 
-  public static int GetAgentCash(byte AgentID)
+  public int GetAgentCash(byte AgentID)
   {
     Agent selectedAgent = Daftar.Find(agent => agent.ID == AgentID);
     return selectedAgent.Cash;
   }
 
-  public static string GetAgentName(byte AgentID)
+  public string GetAgentName(byte AgentID)
   {
     Agent selectedAgent = Daftar.Find(agent => agent.ID == AgentID);
     return selectedAgent.Name;
   }
 
-  public static void AddProperty(byte AgentID, byte PropertyID)
+  public void AddProperty(byte AgentID, byte PropertyID)
   {
     Agent selectedAgent = Daftar.Find(agent => agent.ID == AgentID);
     selectedAgent.Portfolio.Add(new Property(PropertyID, 0));
   }
 
-  public static void RemoveProperty(byte AgentID, byte PropertyID)
+  public void RemoveProperty(byte AgentID, byte PropertyID)
   {
     Agent agent = Daftar.Find(agent => agent.ID == AgentID);
     Property propertyToRemove = agent.Portfolio.Find(p => p.ID == PropertyID);
     agent.Portfolio.Remove(propertyToRemove);
   }
 
-  public static byte CheckForOwnership(byte PropertyID)
+  public byte CheckForOwnership(byte PropertyID)
   {
     foreach (Agent agent in Daftar) if (agent.Portfolio.Any(property => property.ID == PropertyID)) { return agent.ID; }
     
@@ -92,7 +91,7 @@ public partial class Khana : CSharpScript
     return 69;
   }
 
-  public static void ToggleMortgageStatus(byte AgentID, byte PropertyID, bool newStatus)
+  public void ToggleMortgageStatus(byte AgentID, byte PropertyID, bool newStatus)
   {
     Agent agent = Daftar.Find(agent => agent.ID == AgentID);
     Property property = agent.Portfolio.Find(p => p.ID == PropertyID);
@@ -100,7 +99,7 @@ public partial class Khana : CSharpScript
     property.IsMortgaged = newStatus;
   }
 
-  public static void ModifyUpgradeLvl(byte AgentID, byte PropertyID, byte newLvl)
+  public void ModifyUpgradeLvl(byte AgentID, byte PropertyID, byte newLvl)
   {
     Agent agent = Daftar.Find(agent => agent.ID == AgentID);
     Property property = agent.Portfolio.Find(p => p.ID == PropertyID);
@@ -108,7 +107,7 @@ public partial class Khana : CSharpScript
     property.UpgradeLevel = newLvl;
   }
   
-  public static byte[] GetAgentPortfolio(byte AgentID)
+  public byte[] GetAgentPortfolio(byte AgentID)
   {
     List<byte> propertyIDs = new();
     Agent agent = Daftar.Find(agent => agent.ID == AgentID);
