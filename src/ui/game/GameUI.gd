d9432d255293d7c14ca98e171c4d38e12dee0658
DipShtick <vancouver_cliparf@simplelogin.com>
2023-12-04 17:59:14 +0100

    Update GameUI.gd

--- src/ui/game/GameUI.gd
+++ src/ui/game/GameUI.gd
@@ -6,12 +6,12 @@ signal RequestPause
 signal RequestTrade
 signal RequestPrison
 
-func _onRequest(functionID, secondOption, agentID):
+func _onRequest(functionID, secondOption, thirdOption):
 	match functionID:
 		"PAUSE": emit_signal("RequestPause")
-		"PROP": emit_signal("RequestCard", "PROP", secondOption, agentID)
+		"PROP": emit_signal("RequestCard", "PROP", secondOption, thirdOption)
 		"CHEST": emit_signal("RequestCard", "CHEST", secondOption)
 		"CHANCE": emit_signal("RequestCard", "CHANCE", secondOption)
 		"DICE": emit_signal("RequestDice", secondOption)
-		"PRISON": emit_signal("RequestPrison")
-		"TRADE": emit_signal("RequestTrade")
+		"PRISON": emit_signal("RequestPrison", secondOption)
+		"TRADE": emit_signal("RequestTrade", secondOption, thirdOption)
