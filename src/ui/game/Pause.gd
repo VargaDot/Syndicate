08d5c9d6d18b74a5185fd18df5b7c795eea647c8
DipShtick <vancouver_cliparf@simplelogin.com>
2023-12-05 21:13:23 +0100

    Pause menu updated
    Added 4 buttons
    Connected their signals

--- src/ui/game/Pause.gd
+++ src/ui/game/Pause.gd
@@ -3,3 +3,15 @@ extends Control
 func _onRequest():
 	if visible: hide()
 	else: show()
+
+func _on_save_pressed():
+	pass # Replace with function body.
+
+func _on_load_pressed():
+	pass # Replace with function body.
+
+func _on_soft_quit_pressed():
+	pass # Replace with function body.
+
+func _on_hard_quit_pressed():
+	pass # Replace with function body.
