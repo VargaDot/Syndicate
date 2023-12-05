173062abe2e084f1afd2158c5dce977caf8e2f86
DipShtick <vancouver_cliparf@simplelogin.com>
2023-12-05 20:47:07 +0100

    closePressed method removed
    switched in favour of _singlePlayerPressed

--- src/ui/mainmenu/MainMenu.gd
+++ src/ui/mainmenu/MainMenu.gd
@@ -7,19 +7,16 @@ func _process(_delta):
 	if Input.is_action_pressed("Quit"): get_tree().quit()
 	if Input.is_action_just_pressed("SkipAnimation") && animPlyr.is_playing(): animPlyr.seek(1, true)
 
-var shown:bool
+var singleShown:bool
 func _singlePlayerPressed() -> void:
-	if shown: animPlyr.play("HideSingleplayer")
+	if singleShown: animPlyr.play("HideSingleplayer")
 	else: animPlyr.play("ShowSingleplayer")
-	shown = !shown
+	singleShown = !singleShown
 
 func _multiPressed() -> void:
 	pass
 
-func _closePressed() -> void:
-	animPlyr.play("HideSingleplayer")
-	shown = false
-
 func _on_completion():
 	Composer.disable_scene("MainMenu")
 	Composer.activate_scene("Game")
+
