db8bf7005daa2fa115892ca870c33a78c694fc7d
DipShtick <vancouver_cliparf@simplelogin.com>
2023-12-06 18:23:30 +0100

    bid & fold buttons connected

--- src/ui/game/Auction.gd
+++ src/ui/game/Auction.gd
@@ -2,3 +2,9 @@ extends Control
 
 func _on_start_auction(firstAgent, internalName):
 	pass # Replace with function body.
+
+func _on_bid_pressed():
+	pass # Replace with function body.
+
+func _on_fold_pressed():
+	pass # Replace with function body.
