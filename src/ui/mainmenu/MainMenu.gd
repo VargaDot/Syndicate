ebaeea7d1c332ea30e79511bb9e4036a4578d8be
DipShtick <vancouver_cliparf@simplelogin.com>
2023-12-06 18:02:30 +0100

    x:bool = false --> x.visible

--- src/ui/mainmenu/MainMenu.gd
+++ src/ui/mainmenu/MainMenu.gd
@@ -7,11 +7,13 @@ func _process(_delta):
 	if Input.is_action_pressed("Quit"): get_tree().quit()
 	if Input.is_action_just_pressed("SkipAnimation") && animPlyr.is_playing(): animPlyr.seek(1, true)
 
-var singleShown:bool = false
 func _singlePlayerPressed() -> void:
-	if singleShown: animPlyr.play("HideSingleplayer")
-	else: animPlyr.play("ShowSingleplayer")
-	singleShown = !singleShown
+	if $SingleplayerMenu.visible: 
+		animPlyr.play("HideSingleplayer")
+		$SingleplayerMenu.hide()
+	else:
+		$SingleplayerMenu.show()
+		animPlyr.play("ShowSingleplayer")
 
 func _multiPressed() -> void:
 	pass
@@ -20,8 +22,10 @@ func _on_completion():
 	Composer.disable_scene("MainMenu")
 	Composer.activate_scene("Game")
 
-var creditsShown:bool = false
 func _on_credits_pressed():
-	if creditsShown: animPlyr.play("ShowCredits")
-	else: animPlyr.play("HideCredits")
-	creditsShown = !creditsShown
+	if $Credits.visible:
+		animPlyr.play("HideCredits")
+		$Credits.hide()
+	else: 
+		animPlyr.play("ShowCredits")
+		$Credits.show()
