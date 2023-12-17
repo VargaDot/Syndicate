d3833aac35d003abf622b5ba1b8ded3bc70e7070
VargaDot <vancouver_cliparf@simplelogin.com>
2023-12-17 20:52:27 +0100

    Next turn button

--- src/Game.gd
+++ src/Game.gd
@@ -9,28 +9,36 @@ signal RequestWin()
 signal RequestLoss()
 
 func _ready(): _turnManager()
-func _process(_delta): if Input.is_action_pressed("Quit"): emit_signal("RequestPause")
+func _process(_delta): 
+	if Input.is_action_pressed("Quit"): emit_signal("RequestPause")
+	match currentGame:
+		GAME_STATES.ROLL: pass
+		GAME_STATES.BANKRUPT: pass
+		GAME_STATES.COMPLETE: pass
+		_: pass
+
+enum GAME_STATES {NORMAL, ROLL, BANKRUPT, COMPLETE}
+var currentGame = GAME_STATES.NORMAL
+
+enum DISTRICT_TYPE { GO, PROPERTY, CHEST, CHANCE, ITAX, LTAX, JAIL, GOJAIL, PARKING }
 
 var firstRound:bool = true
 var currentPlayer:int = 0
 var agentCount:int
 func _turnManager():
-	agentCount = Khana.AgentCount()
+	agentCount = Khana.AgentCount() # Create some sort of array or list instead
 	
-	if firstRound: 
+	if firstRound:
 		currentPlayer = randi_range(1, agentCount)
 		firstRound = false
 	else: currentPlayer = (currentPlayer + 1) % agentCount
 	
 	if agentCount == 1: emit_signal("RequestWin", currentPlayer)
-	else: pass
-	
 	if Khana.GetAgentStatus(currentPlayer) == true: emit_signal("RequestPrison")
-	else: pass
 	
 	var roll = randi_range(2, 12)
 	if roll % 2 == 0: Khana.ModifyDoubleCount(currentPlayer, false)
-	else: 
+	else:
 		Khana.ModifyDoubleCount(currentPlayer, true)
 		if Khana.GetAgentDoublesCount(currentPlayer) == 3: 
 			Khana.ToggleAgentFreedom(currentPlayer)
@@ -39,43 +47,41 @@ func _turnManager():
 	
 	emit_signal("RequestDice", roll)
 	Khana.MoveAgent(currentPlayer, roll)
-	_tileInspector()
-
-enum districtTypes { GO, PROPERTY, CHEST, CHANCE, ITAX, LTAX, JAIL, GOJAIL, PARKING }
-
-func _tileInspector(): 
+	
 	var pos:int = Khana.GetAgentPosition(currentPlayer)
 	var tiletype = EstateCourt.FetchDistrictData(pos, "TYPE")
 	var bankrupter:int = 257
 	
 	match tiletype:
-		districtTypes.GO: Khana.ConductTransaction(currentPlayer, 200)
-		districtTypes.PROPERTY:
+		DISTRICT_TYPE.GO: Khana.ConductTransaction(currentPlayer, 200)
+		DISTRICT_TYPE.PROPERTY:
 			var propowner:int = Khana.CheckForOwnership(pos)
 			if propowner == 69: emit_signal("RequestCard", "PROP", pos, currentPlayer)
+			elif propowner != 69: if Khana.GetMortgageStatus(propowner, pos) == true: pass
 			else:
-				if Khana.GetMortgageStatus(propowner, pos) == true: pass
-				else: 
-					var proplevel:int = Khana.GetUpgradeLevel(currentPlayer, pos)
-					var internalName:String = EstateCourt.FetchDistrictData(pos, "NAME")
-					var propPrice:int = EstateCourt.FetchAssetData(internalName, "RENT", proplevel)
-					Khana.ConductTransaction(currentPlayer, -propPrice)
-					Khana.ConductTransaction(propowner, proplevel)
-					bankrupter = propowner
-		districtTypes.CHEST: emit_signal("RequestCard", "CHEST")
-		districtTypes.CHANCE: emit_signal("RequestCard", "CHANCE")
-		districtTypes.ITAX: Khana.ConductTransaction(currentPlayer, -roundi(Khana.GetAgentCash() * 0.1))
-		districtTypes.LTAX: Khana.ConductTransaction(currentPlayer, -100)
-		districtTypes.JAIL: pass
-		districtTypes.GOJAIL: Khana.ToggleAgentFreedom(currentPlayer)
-		districtTypes.PARKING: pass
+				var proplevel:int = Khana.GetUpgradeLevel(currentPlayer, pos)
+				var internalName:String = EstateCourt.FetchDistrictData(pos, "NAME")
+				var propPrice:int = EstateCourt.FetchAssetData(internalName, "RENT", proplevel)
+				Khana.ConductTransaction(currentPlayer, -propPrice)
+				Khana.ConductTransaction(propowner, proplevel)
+				bankrupter = propowner
+		DISTRICT_TYPE.CHEST: emit_signal("RequestCard", "CHEST")
+		DISTRICT_TYPE.CHANCE: emit_signal("RequestCard", "CHANCE")
+		DISTRICT_TYPE.ITAX: Khana.ConductTransaction(currentPlayer, -roundi(Khana.GetAgentCash() * 0.1))
+		DISTRICT_TYPE.LTAX: Khana.ConductTransaction(currentPlayer, -100)
+		DISTRICT_TYPE.JAIL: pass
+		DISTRICT_TYPE.GOJAIL: Khana.ToggleAgentFreedom(currentPlayer)
+		DISTRICT_TYPE.PARKING: pass
 		_: print("not found")
 	
-	_check_for_bankruptcy(bankrupter)
-
-signal file_bankruptcy()
-func _check_for_bankruptcy(bankrupter):
 	if Khana.GetAgentCash(currentPlayer) < 0:
-		emit_signal("file_bankruptcy", currentPlayer, bankrupter)
+		emit_signal("RequestLoss", currentPlayer, bankrupter)
 		Khana.RemoveAgent(currentPlayer)
-	else: pass 
+	
+	$UI/NextTurn.show()
+	if Input.is_action_pressed("Confirm"): _on_next_turn_pressed()
+	
+
+func _on_next_turn_pressed():
+	_turnManager()
+	$UI/NextTurn.hide()
