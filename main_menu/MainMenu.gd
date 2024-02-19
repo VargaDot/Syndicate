09de9063006f73a95bc467d2a94b38e33b3c9206
VargaDot <vancouver_cliparf@simplelogin.com>
2024-02-19 15:31:31 +0100

    Update MainMenu.gd

--- main_menu/MainMenu.gd
+++ main_menu/MainMenu.gd
@@ -4,13 +4,16 @@ extends Control
 @onready var animPlyr:AnimationPlayer = $AnimationPlayer
 
 func _process(_delta):
-	if Input.is_action_pressed("Quit"): get_tree().quit()
-	if Input.is_action_just_pressed("SkipAnimation") && animPlyr.is_playing(): animPlyr.seek(1, true)
+	if Input.is_action_pressed("Quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("SkipAnimation") && animPlyr.is_playing():
+		animPlyr.seek(1, true)
 
 func _singlePlayerPressed() -> void:
 	if $SingleplayerMenu.visible: 
 		animPlyr.play("HideSingleplayer")
-		while animPlyr.is_playing(): pass
+		while animPlyr.is_playing():
+			pass
 		$SingleplayerMenu.hide()
 	else:
 		$SingleplayerMenu.show()
@@ -26,7 +29,8 @@ func _on_completion():
 func _on_credits_pressed():
 	if $Credits.visible:
 		animPlyr.play("HideCredits")
-		while animPlyr.is_playing(): pass
+		while animPlyr.is_playing():
+			pass
 		$Credits.hide()
 	else: 
 		animPlyr.play("ShowCredits")
